066b66c6d18e2d7ba84de5f439462f93495ea839
impta <87300042+impta@users.noreply.github.com>
2022-06-14 14:43:37 +0430

    Update tguserbot.sh

--- tguserbot.sh
+++ tguserbot.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/impta/VCPlayerBot/ /root/VCPlayerBot
-cp CREDS/config.ini /root/VCPlayerBot/config.ini
+cp CREDS/.env /root/VCPlayerBot/config.ini
 cd /root/VCPlayerBot
 pip3 install -r requirements.txt
 sudo bash install_node.sh
+sudo bash install_python39.sh
+sudo bash install_ffmpeg.sh
 python3 main.py
