5425fc857e2001a39f53717a06a8d2aeb07b897e
impta <87300042+impta@users.noreply.github.com>
2022-06-16 14:16:40 +0430

    Update hubbax.sh

--- hubbax.sh
+++ hubbax.sh
@@ -1,7 +1,8 @@
-git clone https://github.com/impta/VCPlayerBot/ /root/hubk
-cp CREDS/.env /root/hubk/.env
-cd /root/hubk
+git clone https://github.com/impta/VCPlayerBot/ /root/hubt
+cp CREDS/.env /root/hubt/.env
+cd /root/hubt
 pip3 install -r requirements.txt
 sudo bash install_node.sh
+sudo bash install_python39.sh
 sudo bash install_ffmpeg.sh
 python3 main.py
